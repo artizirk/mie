9d4013f2475b9e3c4b40bcc6e97805832aba1aea
Arti Zirk <arti.zirk@gmail.com>
2016-04-23 09:56:16 +0300

    New web app code

--- web/js/app.js
+++ web/js/app.js
@@ -1,60 +1,83 @@
 var env = new nunjucks.Environment(new nunjucks.WebLoader('/templates', {async: true}), {autoescape: true});
 
-var index = {
-    page: "index.html",
+function Page(){}
+Page.prototype.load = function() {
+    this.render();
+};
+Page.prototype.render = function(data) {
+    console.log(this)
+    env.render(this.page, data, this.display.bind(this))
+};
+Page.prototype.display = function(err, res) {
+    console.log(this)
+    if (err) {
+        console.error(err);
+        return;
+    }
+    $("#content").html(res);
+    this.ondisplay(err, res)
+};
+Page.prototype.ondisplay = function(){}
+Page.prototype.on = function(event, selector, callback) {
+    if (!('events' in this)) {
+        this.events = [];
+    }
+    console.log(this);
 
-    render: function(){
-        data = { foo: 'bar' };
-        env.render(this.page, data, this.display.bind(this));
-    },
-
-    display: function(err, res){
-        if (err) {
-            console.log(err);
-            return;
-        }
-        $("#content").html(res);
-        $("#content").on("click", "a", this.load_museum);
-    },
-
-    load_museum: function() {
-        console.log("loading  museum");
-        $("#content").off("click", "a", this.load_museum);
-        museum.render();
-        return false;
-    }.bind(this),
+    this.events.push([event, selector, callback])
+    $("#content").on(event, selector, callback);
+}
+Page.prototype.alloff = function() {
+    for (var i = this.events.length - 1; i >= 0; i--) {
+        var event = this.events[i];
+        $("#content").off(event[0], event[1], event[2]);
+    }
 }
 
-var museum = {
-    page: "museum.html",
-
-    render: function(){
-        data = { name: "A Museum" };
-        env.render(this.page, data, this.display.bind(this));
-    },
-
-    display: function(err, res) {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        $("#content").html(res);
-        $("#content").on("click", "a", this.load_index);
-    },
-
-    load_index: function(){
-        console.log("load index")
-        $("#content").off("click", "a", this.load_index);
-        index.render();
-        return false;
-    }.bind(this)
-
-
+function Index(app) {
+    this.page = "index.html";
+    this.app = app;
 }
+Index.prototype = Object.create(Page.prototype);
+Index.prototype.load = function() {
+    this.render({ foo:"bar"});
+};
+Index.prototype.ondisplay = function(err, res) {
+    this.on("click", "a", this.load_museum.bind(this));
+}
+Index.prototype.load_museum = function() {
+    console.log("loading  museum");
+    this.alloff();
+    this.app.museum.load();
+    return false;
+}
+
+function Museum(app) {
+    this.page = "museum.html";
+    this.app = app;
+}
+Museum.prototype = Object.create(Page.prototype);
+Museum.prototype.load = function(){
+    this.render({name: "A Museum"});
+}
+Museum.prototype.ondisplay = function(err, res) {
+    this.on("click", "a", this.load_index.bind(this));
+}
+Museum.prototype.load_index = function(){
+    console.log("load index")
+    this.alloff()
+    this.app.index.load();
+    return false;
+}
+
+
 
 $(document).ready(function(){
     console.log("init");
-    index.render();
+    var page = {}
+    page.index = new Index(page);
+    page.museum = new Museum(page);
+    page.index.load();
 
 });
 
